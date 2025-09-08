dd082428439ee88b9d5b720f8eb8f10235235f61
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-09-08 20:21:36 +0100

    Begin Loader

--- Detour/assets/sdk.js
+++ Detour/assets/sdk.js
@@ -6,16 +6,65 @@ var kindle = window.kindle || top.kindle;
 
 window.detour = {
     version: 1,
-    alert(text) {
-        kindle.messaging.sendMessage("com.lab126.pillow", "pillowAlert", { "clientParams": { "alertId": text, "show": true } });
+    getFile(url) {
+            return new Promise(function (resolve) {
+                var iframe = document.createElement("iframe");
+                iframe.src = url;
+                document.body.appendChild(iframe);
+                iframe.addEventListener("load", function (e) {
+                    var src = e.target.contentDocument.documentElement.innerHTML;
+                    e.target.remove();
+                    resolve(
+                        src
+                            .replace(
+                                '<head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">',
+                                ""
+                        )
+                        .replace("</pre></body>", "")
+                        .replace("<head></head><body></body>", "")
+                );
+            });
+            setTimeout(function () {
+                iframe.remove();
+            }, 2000);
+        });
     },
-    getFile() {
+    getDirectory(loc) {
+        return new Promise(function (callback) {
+            _file(loc).then(function (data) {
+                var files = [];
+                var tagReg = new RegExp('<a(\n|.)*?(?=<\/a>)', "gim");
+                var hrefReg = new RegExp('href="(\n|.)*?(?=")', "gim");
+                var tagRemovalReg = new RegExp('<a(\n|.)*?>', "gim");
 
+                var anchorTags = Array.prototype.slice.call(data.matchAll(tagReg));
+
+                for (var i = 0; i < anchorTags.length; i++) {
+                    var source = Array.prototype.slice.call(
+                        anchorTags[i][0].matchAll(hrefReg)
+                    );
+
+                    files.push({
+                        name: anchorTags[i][0].replace(tagRemovalReg, ""),
+                        path: source[0][0].substring(6),
+                    });
+                }
+
+                callback(files);
+            });
+        });
     },
-    getDirectory() {
+    joinPath(one, two) {
+        if (one.slice(-1) === "/") {
+            one = one.slice(0, -1);
+        };
 
-    },
-    joinPath() {
+        if (two.slice(0, 2) === "./") {
+            two = two.slice(1);
+        } else if (two[0] !== "/") {
+            two = "/" + two;
+        };
 
+        return one + two;
     }
 };
