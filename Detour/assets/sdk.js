91a4d561d8aa714b36924c28c73dc532d2c62cfc
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-09-09 20:39:44 +0100

    FULL mesquito sdk. WHAT THE HELL.

--- Detour/assets/sdk.js
+++ Detour/assets/sdk.js
@@ -1,63 +1,83 @@
-var kindle = window.kindle || top.kindle;
+function getFile(url, timeout, fixKindleFormatting) {
+  if (typeof (fixKindleFormatting) === 'undefined') {
+    fixKindleFormatting = true
+  }
 
-function getFile(url) {
-    return new Promise(function (resolve) {
-        var iframe = document.createElement("iframe");
-        iframe.src = url;
-        document.body.appendChild(iframe);
+  return new Promise(function (callback) {
+    // Create iframe
+    const iframe = document.createElement("iframe");
+    iframe.src = url;
 
-        iframe.addEventListener("load", function (e) {
-            try {
-                var src = e.target.contentDocument.documentElement.innerHTML;
-                e.target.remove();
+    // Add iframe to body
+    document.body.appendChild(iframe);
 
-                src = src
-                    .replace('<head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">', "")
-                    .replace("</pre></body>", "")
-                    .replace("<head></head><body></body>", "");
+    // Timeout To Remove iframe
+    if (timeout !== -1) {
+      setTimeout(function () {
+        iframe.remove();
+      }, timeout || 2000);
+    }
 
-                resolve(src);
-            } catch(err) {
-                console.error("Failed To Read <iframe> Content:", err);
-                resolve("");
-            }
-        });
+    // Add listener to iframe
+    iframe.addEventListener("load", function (event) {
+      // Get iframe's documentElement and clone it and get the innerHTML
+      iframeSource = event.target.contentDocument.documentElement.innerHTML;
 
-        setTimeout(function () {
-            if (iframe.parentNode) iframe.remove();
-        }, 2000);
+      // Remove iframe from DOM
+      event.target.remove();
+
+      if (fixKindleFormatting) {
+        // (replace applies to a lot of file types, not just directory listings)
+        iframeSource = iframeSource.replace('<head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">', '').replace('</pre></body>', '');
+      }
+
+      // Resolve promise
+      callback(iframeSource);
     });
+  });
 }
 
-function getDirectory(loc) {
-    return new Promise(function (resolve) {
-        getFile(loc).then(function (data) {
-            var files = [];
-            var tagReg = /<a(\n|.)*?(?=<\/a>)/gim;
-            var hrefReg = /href="(\n|.)*?(?=")/gim;
-            var tagRemovalReg = /<a(\n|.)*?>/gim;
 
-            var anchorTags = Array.from(data.matchAll(tagReg));
+function getDirectory(location) {
+  return new Promise(function (callback) {
+    // Get source of directory listing
+    getFile(location).then(function (data) {
+      // Create list
+      var files = [];
 
-            for (var i = 0; i < anchorTags.length; i++) {
-                var source = Array.from(anchorTags[i][0].matchAll(hrefReg));
-                files.push({
-                    name: anchorTags[i][0].replace(tagRemovalReg, ""),
-                    path: source[0][0].substring(6)
-                });
-            }
+      // Define regex for extracting different things
+      const tagReg = new RegExp('<a(\n|.)*?(?=<\/a>)', "gim");
+      const hrefReg = new RegExp('href="(\n|.)*?(?=")', 'gim');
+      const tagRemovalReg = new RegExp('<a(\n|.)*?>', 'gim');
 
-            resolve(files);
-        }).catch(function (err) {
-            console.error("Error Reading Directory:", err);
-            resolve([]);
+      // Get anchor tags (without closing tag)
+      const anchorTags = Array.from(data.matchAll(tagReg));
+
+      for (var i = 0; i < anchorTags.length; i++) {
+        // Get href source (URL)
+        const source = Array.from(anchorTags[i][0].matchAll(hrefReg));
+
+        files.push({
+          "name": anchorTags[i][0].replace(tagRemovalReg, ''), // Remove anchor tags from match to get only their innerText
+          "path": source[0][0].substring(6) // Remove the href=" part of the href source
         });
+      }
+
+      callback(files); // Resolve promise
     });
+  });
 }
 
-function joinPaths(one, two) {
-    if (one.slice(-1) === "/") one = one.slice(0, -1);
-    if (two.slice(0, 2) === "./") two = two.slice(1);
-    else if (two[0] !== "/") two = "/" + two;
-    return one + two;
+function joinPaths(path1, path2) {
+  if (path1.slice(-1) == '/') {
+    path1 = path1.slice(0, -1);
+  }
+
+  if (path2.slice(0, 2) == './') {
+    path2 = path2.slice(1);
+  } else if (path2[0] != '/') {
+    path2 = '/' + path2;
+  }
+
+  return path1 + path2;
 }
