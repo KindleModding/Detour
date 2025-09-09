2f4f4c4cea810bdcf994204e5338dcfaf55848a5
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-09-09 20:14:10 +0100

    HELP ME.

--- Detour/script.js
+++ Detour/script.js
@@ -58,86 +58,9 @@ window.kindle.messaging.receiveMessage("systemMenuItemSelected", function(type, 
     };
 });
 
-// WAF Loading Parts
-function log(msg) {
-  var wafsEl = document.getElementById("wafs");
-  var div = document.createElement("div");
-  div.textContent = msg;
-  wafsEl.appendChild(div);
-}
-
+//WAF Loading Parts
 document.addEventListener("DOMContentLoaded", function () {
-  var wafsEl = document.getElementById("wafs");
-  wafsEl.innerHTML = "";
-
-  log("Detour: Starting scan of /mnt/us/Apps");
-
-  window.detour.getDirectory("file:///mnt/us/Apps").then(function (data) {
-    log("Directories found:\n" + JSON.stringify(data, null, 2));
-
-    for (var d = 0; d < data.length; d++) {
-      (function (directory) {
-        log("Checking directory: " + JSON.stringify(directory));
-
-        var configPath = window.detour.joinPaths(directory.path, "config.xml");
-        log("Looking for config.xml at: " + configPath);
-
-        window.detour.getFile(configPath).then(function (xml) {
-          log("Got config.xml for " + directory.name + ":\n" + xml.substring(0, 200) + "...");
-
-          // Parse XML
-          var parser = new DOMParser();
-          var doc = parser.parseFromString(xml, "text/xml");
-
-          // Check for parse errors
-          if (doc.getElementsByTagName("parsererror").length > 0) {
-            log("XML parse error in " + configPath);
-          }
-
-          // Get WAF Name
-          var names = doc.getElementsByTagName("name");
-          var appName = null;
-          for (var i = 0; i < names.length; i++) {
-            log("Found <name>: " + names[i].textContent);
-            if (names[i].getAttribute("xml:lang") === "en") {
-              appName =
-                names[i].textContent ||
-                (names[i].firstChild && names[i].firstChild.nodeValue);
-              break;
-            }
-          }
-          log("Final appName: " + appName);
-
-          // Get WAF Entrypoint
-          var contents = doc.getElementsByTagName("content");
-          var payload = "index.html"; // Fallback
-          if (contents.length > 0) {
-            var srcAttr = contents[0].getAttribute("src");
-            log("Found content src: " + srcAttr);
-            if (srcAttr) {
-              payload = srcAttr;
-            }
-          }
-          log("Payload for " + (appName || directory.name) + ": " + payload);
-
-          // Make Button
-          var link = document.createElement("button");
-          link.innerText = appName || directory.name;
-          link.onclick = function () {
-            var target = window.detour.joinPaths(directory.path, payload);
-            log("Redirecting to: " + target);
-            window.location.href = target;
-          };
-
-          // Append
-          wafsEl.appendChild(link);
-          log("Button appended for " + (appName || directory.name));
-        }).catch(function (err) {
-          log("Error reading config.xml in " + directory.path + ": " + err);
-        });
-      })(data[d]);
-    }
-  }).catch(function (err) {
-    log("Error getting directory listing: " + err);
+  getDirectory("file:///mnt/us/Apps").then(function (data) {
+    document.querySelector("#wafs").innerHTML = JSON.stringify(data); 
   });
 });
